0884abee48e207ac862da639272b5d8cfe852934
Dean Richards <deanrichardsdeveloper@gmail.com>
2021-11-11 14:30:49 +0000

    add mixins and vars

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -2,6 +2,22 @@ const Home = () => {
 	return (
 		<div className="home">
 			<p>this is the home page</p>
+			<p>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit
+				animi possimus eveniet, optio iusto modi aliquam tempore,
+				asperiores veritatis accusamus, earum similique natus
+				consectetur exercitationem iste ex consequatur incidunt
+				perspiciatis repellat laborum eos sapiente temporibus. Tempore
+				asperiores veritatis cumque dolore ab animi, placeat explicabo
+				consectetur quae praesentium. Ea blanditiis commodi ullam
+				suscipit laborum placeat similique delectus! Enim placeat nulla
+				eum dolor inventore aliquam quisquam ab reiciendis ullam iste?
+				Excepturi est minus nesciunt id repellendus inventore labore
+				commodi culpa eaque qui nihil exercitationem molestiae quam
+				nostrum nisi dignissimos, dolore facilis ab laudantium pariatur
+				perspiciatis dolorem architecto. Numquam asperiores repudiandae
+				fugit dolore?
+			</p>
 		</div>
 	);
 };
